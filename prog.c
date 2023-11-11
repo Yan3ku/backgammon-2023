8a4f07414fb63351e7f4ab881aa6a9eca3063e3c
Yan3ku <wisniewskijas@outlook.com>
2023-11-11 10:46:21 +0100

    Remove boarder window

--- prog.c
+++ prog.c
@@ -18,26 +18,25 @@ enum {
 enum { /* spaces */
 	PILEH_OFF  = 2,
 	PILEV_OFF  = 6,
-	GAPH_OFF = 2,
-	GAPV_OFF = 3,
-	BAR_OFF  = 3
+	GAPH_OFF   = 2,
+	GAPV_OFF   = 3,
+	BAR_OFF    = 3
 };
 
 enum { /* windows */
 	WBOARDER     = 2,
 	WBOARD_TOP   = 2,
-	WBOARD_LINES = PILEV_OFF * 2 + GAPV_OFF,
-	WBOARD_COLS  = PILEH_OFF * 12 + GAPH_OFF * 14 + BAR_OFF,
-	WBOARD_BOT   = WBOARD_LINES + WBOARD_TOP + WBOARDER + 1,
-	WROOT_LINES  = WBOARD_LINES + WBOARDER + 6,
-	WROOT_COLS   = WBOARD_COLS  + WBOARDER + 6,
+	WBOARD_LINES = PILEV_OFF * 2 + GAPV_OFF + WBOARDER,
+	WBOARD_COLS  = PILEH_OFF * 12 + GAPH_OFF * 14 + BAR_OFF + WBOARDER,
+	WBOARD_BOT   = WBOARD_LINES + WBOARD_TOP + 1,
+	WROOT_LINES  = WBOARD_LINES + 6,
+	WROOT_COLS   = WBOARD_COLS  + 6,
 };
 
 typedef struct {
 	int points[24][15];
 	int bar[30];
 	WINDOW *wroot;
-	WINDOW *wboarder;
 	WINDOW *wboard;
 } Board;
 
@@ -60,14 +59,15 @@ setup()
  * <gap> 6*(<plc> <gap>) <bar> <gap> 6*(<plc> <gap>)
  * VERTICAL:
  * 5*<plc> <counter> <gap> <counter> 5*<plc>
+ * +2 for outside border
  * This is important for "plctoscr" function (place to screen)
  */
 void
 plctoscr(int plc, int i, int *x, int *y) {
 	int col = abs(11 - plc) - (plc > 11);
 	int bar = col < 6 ? 0 : BAR_OFF + GAPH_OFF;
-	*y = GAPH_OFF + col * (GAPH_OFF + PILEH_OFF) + bar;
-	*x = (plc < 12 ? WBOARD_LINES - i - 1 : i);
+	*y = GAPH_OFF + col * (GAPH_OFF + PILEH_OFF) + bar + 1;
+	*x = (plc < 12 ? WBOARD_LINES - i - 2 : i + 1);
 }
 
 void
@@ -77,17 +77,15 @@ boarddrw(Board *board)
 	int x, y;
 
 	board->wroot    = newwin(WROOT_LINES, WROOT_COLS,
-	                        (LINES - WROOT_LINES) / 2,
-	                        (COLS  - WROOT_COLS)  / 2);
-	board->wboarder = derwin(board->wroot, WBOARD_LINES + 2, WBOARD_COLS + 2, WBOARD_TOP + 1, 0);
-	board->wboard   = derwin(board->wboarder, WBOARD_LINES, WBOARD_COLS, 1, 1);
-	wborder(board->wboarder, ':', ':', '=', '=', '=', '=', '=', '=');
+	                         (LINES - WROOT_LINES) / 2,
+	                         (COLS  - WROOT_COLS)  / 2);
+	board->wboard   = derwin(board->wroot, WBOARD_LINES, WBOARD_COLS, WBOARD_TOP + 1, 1);
+	wborder(board->wboard, ':', ':', '=', '=', '=', '=', '=', '=');
 	mvwprintw(board->wroot, 0, 0, TITLE);
 
 	for (i = 0; i < nelem(board->points); i++)  {
 		for (j = 0; j < 6; j++) {
 			plctoscr(i, j, &x, &y);
-			wattron(board->wboard, A_BOLD);
 			mvwprintw(board->wboard, x, y, PLC_SYM(i));
 		}
 		mvwprintw(board->wroot, i < 12 ? WBOARD_BOT : WBOARD_TOP, y + 1, "%02ld", i + 1);
@@ -107,7 +105,6 @@ main()
 	refresh();
 	wrefresh(board->wroot);
 	wrefresh(board->wboard);
-	wrefresh(board->wboarder);
 	getch();
 	endwin();
 	free(board);
